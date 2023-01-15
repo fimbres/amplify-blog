94643925f786ac6a76aff95f62bd48ab18b8ba13
Isaac <fimbresisaac23@gmail.com>
2023-01-15 13:36:38 -0800

    fixing styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head'
 import { API, graphqlOperation } from 'aws-amplify'
 import Link from 'next/link';
+import ReactMarkDown from 'react-markdown'
 import moment from 'moment';
 
 import { listTodos } from 'src/graphql/queries';
@@ -35,15 +36,15 @@ export default function Home() {
         <div className='max-w-xl w-full mx-auto pt-28'>
           <div className='text-4xl font-bold text-white'>All Posts</div>
           <div className='min-h-full w-full'>
-            {posts ? (
+            {posts?.listTodos?.items.length ? (
               <div className='flex justify-center items-center min-h-full mt-10 flex-col'>
-                {posts.listTodos?.items.map((post, index) => (
+                {posts.listTodos.items.map((post, index) => (
                   <Link key={index} href={`/posts/${post?.id}`} className='bg-neutral-800 w-full rounded-lg px-5 py-3 mb-7'>
                     <div className='flex space-x-3 items-center'>
                       <div className='text-white text-lg font-medium'>{post?.title}</div>
-                      <div className='text-gray-400 text-sm font-light'>by {post?.username} {moment(post?.updatedAt).fromNow()}</div>
+                      <div className='text-gray-400 text-sm font-light'>by {post?.username ? post?.username : "Unknown"} {moment(post?.updatedAt).fromNow()}</div>
                     </div>
-                    <div className='text-white text-lg'>{post?.content}</div>
+                    <ReactMarkDown className='text-white text-lg'>{post?.content!}</ReactMarkDown>
                   </Link>
                 ))}
               </div>
