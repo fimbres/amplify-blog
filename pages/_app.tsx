4130e25ae64e90f220b093fc19bc87488407f177
Isaac <fimbresisaac23@gmail.com>
2023-01-14 21:39:39 -0800

    new styles for the markdown component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../configureAmplify';
 import 'tailwindcss/tailwind.css'
+import "easymde/dist/easymde.min.css";
+import "../styles/editor.css"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
