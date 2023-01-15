52003daf401ba2f8f6fe1c9f3e3d7e9cb4e82625
Isaac <fimbresisaac23@gmail.com>
2023-01-14 21:44:57 -0800

    adding styles to navbar component

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState} from "react";
 import Link from "next/link";
 import { Auth } from "aws-amplify";
+import { useRouter } from "next/router";
 
 import "../../configureAmplify"; 
 
 const NavBar = () => {
     const [user, setUser] = useState<any | null>(null);
+    const router = useRouter();
     const privateNavLinks = user ? [
         {
             label: "Create Post",
@@ -39,12 +41,16 @@ const NavBar = () => {
     }, []);
 
     return (
-        <nav className="flex justify-between fixed top-0 right-0 left-0 w-full py-4 px-8 bg-neutral-800 border-b">
-            <div className="text-xl font-bold text-white">Amplify Blog</div>
+        <nav className="flex justify-between fixed top-0 right-0 left-0 w-full py-4 px-8 bg-neutral-800 border-b border-yellow-500">
+            <div className="text-xl font-bold text-yellow-500">Amplify Blog</div>
             <div className="flex space-x-3">
-                {navLinks.map((link, index) => (
-                    <Link key={index} href={link.to} className="text-xl text-white hover:opacity-70">{link.label}</Link>
-                ))}
+                {navLinks.map((link, index) => {
+                    const isCurrent = link.to === router.pathname;
+
+                    return (
+                        <Link key={index} href={link.to} className={`text-xl hover:opacity-70 ${isCurrent ? "text-yellow-400" : "text-white"}`}>{link.label}</Link>
+                    );
+                })}
             </div>
         </nav>
     )
