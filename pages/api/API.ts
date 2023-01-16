6ebae4ce1a806075e42281b8df870c43f6c99ea1
Isaac <fimbresisaac23@gmail.com>
2023-01-15 18:13:14 -0800

    adding the new Comments table in aws amplify backend

--- pages/api/API.ts
+++ pages/api/API.ts
@@ -67,8 +67,26 @@ export type Todo = {
   content: string,
   username?: string | null,
   coverImage?: string | null,
+  comments?: ModelCommentConnection | null,
   createdAt: string,
   updatedAt: string,
+};
+
+export type ModelCommentConnection = {
+  __typename: "ModelCommentConnection",
+  items:  Array<Comment | null >,
+  nextToken?: string | null,
+};
+
+export type Comment = {
+  __typename: "Comment",
+  id: string,
+  message?: string | null,
+  post?: Todo | null,
+  postID?: string | null,
+  createdAt: string,
+  updatedAt: string,
+  createdBy?: string | null,
 };
 
 export type UpdateTodoInput = {
@@ -83,15 +101,18 @@ export type DeleteTodoInput = {
   id: string,
 };
 
-export type ModelTodoFilterInput = {
-  id?: ModelIDInput | null,
-  title?: ModelStringInput | null,
-  content?: ModelStringInput | null,
-  username?: ModelStringInput | null,
-  coverImage?: ModelStringInput | null,
-  and?: Array< ModelTodoFilterInput | null > | null,
-  or?: Array< ModelTodoFilterInput | null > | null,
-  not?: ModelTodoFilterInput | null,
+export type CreateCommentInput = {
+  id?: string | null,
+  message?: string | null,
+  postID?: string | null,
+};
+
+export type ModelCommentConditionInput = {
+  message?: ModelStringInput | null,
+  postID?: ModelIDInput | null,
+  and?: Array< ModelCommentConditionInput | null > | null,
+  or?: Array< ModelCommentConditionInput | null > | null,
+  not?: ModelCommentConditionInput | null,
 };
 
 export type ModelIDInput = {
@@ -110,6 +131,27 @@ export type ModelIDInput = {
   size?: ModelSizeInput | null,
 };
 
+export type UpdateCommentInput = {
+  id: string,
+  message?: string | null,
+  postID?: string | null,
+};
+
+export type DeleteCommentInput = {
+  id: string,
+};
+
+export type ModelTodoFilterInput = {
+  id?: ModelIDInput | null,
+  title?: ModelStringInput | null,
+  content?: ModelStringInput | null,
+  username?: ModelStringInput | null,
+  coverImage?: ModelStringInput | null,
+  and?: Array< ModelTodoFilterInput | null > | null,
+  or?: Array< ModelTodoFilterInput | null > | null,
+  not?: ModelTodoFilterInput | null,
+};
+
 export type ModelTodoConnection = {
   __typename: "ModelTodoConnection",
   items:  Array<Todo | null >,
@@ -121,6 +163,15 @@ export enum ModelSortDirection {
   DESC = "DESC",
 }
 
+
+export type ModelCommentFilterInput = {
+  id?: ModelIDInput | null,
+  message?: ModelStringInput | null,
+  postID?: ModelIDInput | null,
+  and?: Array< ModelCommentFilterInput | null > | null,
+  or?: Array< ModelCommentFilterInput | null > | null,
+  not?: ModelCommentFilterInput | null,
+};
 
 export type ModelSubscriptionTodoFilterInput = {
   id?: ModelSubscriptionIDInput | null,
@@ -161,6 +212,14 @@ export type ModelSubscriptionStringInput = {
   notIn?: Array< string | null > | null,
 };
 
+export type ModelSubscriptionCommentFilterInput = {
+  id?: ModelSubscriptionIDInput | null,
+  message?: ModelSubscriptionStringInput | null,
+  postID?: ModelSubscriptionIDInput | null,
+  and?: Array< ModelSubscriptionCommentFilterInput | null > | null,
+  or?: Array< ModelSubscriptionCommentFilterInput | null > | null,
+};
+
 export type CreateTodoMutationVariables = {
   input: CreateTodoInput,
   condition?: ModelTodoConditionInput | null,
@@ -174,6 +233,19 @@ export type CreateTodoMutation = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
   } | null,
@@ -192,6 +264,19 @@ export type UpdateTodoMutation = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
   } | null,
@@ -210,8 +295,114 @@ export type DeleteTodoMutation = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
+  } | null,
+};
+
+export type CreateCommentMutationVariables = {
+  input: CreateCommentInput,
+  condition?: ModelCommentConditionInput | null,
+};
+
+export type CreateCommentMutation = {
+  createComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
+  } | null,
+};
+
+export type UpdateCommentMutationVariables = {
+  input: UpdateCommentInput,
+  condition?: ModelCommentConditionInput | null,
+};
+
+export type UpdateCommentMutation = {
+  updateComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
+  } | null,
+};
+
+export type DeleteCommentMutationVariables = {
+  input: DeleteCommentInput,
+  condition?: ModelCommentConditionInput | null,
+};
+
+export type DeleteCommentMutation = {
+  deleteComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
   } | null,
 };
 
@@ -227,6 +418,19 @@ export type GetTodoQuery = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
   } | null,
@@ -248,6 +452,10 @@ export type ListTodosQuery = {
       content: string,
       username?: string | null,
       coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
       createdAt: string,
       updatedAt: string,
     } | null >,
@@ -273,8 +481,108 @@ export type PostsByUsernameQuery = {
       content: string,
       username?: string | null,
       coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
       createdAt: string,
       updatedAt: string,
+    } | null >,
+    nextToken?: string | null,
+  } | null,
+};
+
+export type GetCommentQueryVariables = {
+  id: string,
+};
+
+export type GetCommentQuery = {
+  getComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
+  } | null,
+};
+
+export type ListCommentsQueryVariables = {
+  filter?: ModelCommentFilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
+};
+
+export type ListCommentsQuery = {
+  listComments?:  {
+    __typename: "ModelCommentConnection",
+    items:  Array< {
+      __typename: "Comment",
+      id: string,
+      message?: string | null,
+      post?:  {
+        __typename: "Todo",
+        id: string,
+        title: string,
+        content: string,
+        username?: string | null,
+        coverImage?: string | null,
+        createdAt: string,
+        updatedAt: string,
+      } | null,
+      postID?: string | null,
+      createdAt: string,
+      updatedAt: string,
+      createdBy?: string | null,
+    } | null >,
+    nextToken?: string | null,
+  } | null,
+};
+
+export type CommentsByPostIDQueryVariables = {
+  postID: string,
+  sortDirection?: ModelSortDirection | null,
+  filter?: ModelCommentFilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
+};
+
+export type CommentsByPostIDQuery = {
+  commentsByPostID?:  {
+    __typename: "ModelCommentConnection",
+    items:  Array< {
+      __typename: "Comment",
+      id: string,
+      message?: string | null,
+      post?:  {
+        __typename: "Todo",
+        id: string,
+        title: string,
+        content: string,
+        username?: string | null,
+        coverImage?: string | null,
+        createdAt: string,
+        updatedAt: string,
+      } | null,
+      postID?: string | null,
+      createdAt: string,
+      updatedAt: string,
+      createdBy?: string | null,
     } | null >,
     nextToken?: string | null,
   } | null,
@@ -293,6 +601,19 @@ export type OnCreateTodoSubscription = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
   } | null,
@@ -311,6 +632,19 @@ export type OnUpdateTodoSubscription = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
   } | null,
@@ -329,7 +663,113 @@ export type OnDeleteTodoSubscription = {
     content: string,
     username?: string | null,
     coverImage?: string | null,
+    comments?:  {
+      __typename: "ModelCommentConnection",
+      items:  Array< {
+        __typename: "Comment",
+        id: string,
+        message?: string | null,
+        postID?: string | null,
+        createdAt: string,
+        updatedAt: string,
+        createdBy?: string | null,
+      } | null >,
+      nextToken?: string | null,
+    } | null,
     createdAt: string,
     updatedAt: string,
+  } | null,
+};
+
+export type OnCreateCommentSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentFilterInput | null,
+  createdBy?: string | null,
+};
+
+export type OnCreateCommentSubscription = {
+  onCreateComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
+  } | null,
+};
+
+export type OnUpdateCommentSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentFilterInput | null,
+  createdBy?: string | null,
+};
+
+export type OnUpdateCommentSubscription = {
+  onUpdateComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
+  } | null,
+};
+
+export type OnDeleteCommentSubscriptionVariables = {
+  filter?: ModelSubscriptionCommentFilterInput | null,
+  createdBy?: string | null,
+};
+
+export type OnDeleteCommentSubscription = {
+  onDeleteComment?:  {
+    __typename: "Comment",
+    id: string,
+    message?: string | null,
+    post?:  {
+      __typename: "Todo",
+      id: string,
+      title: string,
+      content: string,
+      username?: string | null,
+      coverImage?: string | null,
+      comments?:  {
+        __typename: "ModelCommentConnection",
+        nextToken?: string | null,
+      } | null,
+      createdAt: string,
+      updatedAt: string,
+    } | null,
+    postID?: string | null,
+    createdAt: string,
+    updatedAt: string,
+    createdBy?: string | null,
   } | null,
 };
