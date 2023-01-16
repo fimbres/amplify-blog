96bb2a5d69f7112199d0deb3d4b18a8ff9d576fd
Isaac <fimbresisaac23@gmail.com>
2023-01-15 16:34:48 -0800

    uploading the coverImage to aws s3

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -1,9 +1,10 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react'
-import { API, Auth } from 'aws-amplify'
+import React, { ChangeEvent, FC, useEffect, useState, useRef } from 'react'
+import { API, Auth, Storage } from 'aws-amplify'
 import { useRouter } from 'next/router';
 import { v4 as uuid } from "uuid";
 import Head from 'next/head';
 import dynamic from 'next/dynamic'
+import Image from 'next/image';
 
 import NavBar from './components/navbar';
 import { createTodo } from '@/src/graphql/mutations';
@@ -14,6 +15,7 @@ interface Post {
     id?: string;
     title: string;
     content: string;
+    coverImage?: string;
 }
 
 const CreatePost: FC = () => {
@@ -21,7 +23,9 @@ const CreatePost: FC = () => {
         title: "",
         content: "",
     });
+    const [image, setImage] = useState<File | null>(null);
     const router = useRouter();
+    const imageFileInput = useRef<HTMLInputElement>(null);
 
     const checkUser = async () => {
         const user = await Auth.currentAuthenticatedUser();
@@ -42,11 +46,30 @@ const CreatePost: FC = () => {
         });
     }
 
+    const uploadImage = async () => {
+        imageFileInput?.current?.click();
+
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const fileUploaded = e?.target?.files ? e.target.files[0] : undefined;
+        
+        if(!fileUploaded) return;
+
+        setImage(fileUploaded);
+    }
+
     const createPost = async () => {
         if(!post.title || !post.content) return;
 
         const id = uuid();
         post.id = id;
+
+        if(image) {
+            const filename = `${image.name}_${uuid()}`;
+            post.coverImage = filename;
+            await Storage.put(filename, image);
+        }
 
         await API.graphql({
             query: createTodo,
@@ -64,11 +87,14 @@ const CreatePost: FC = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={`min-h-screen w-full bg-neutral-900 pt-60`}>
+            <main className={`min-h-screen w-full bg-neutral-900 pt-32`}>
                 <NavBar />
                 <div className='max-w-xl w-full mx-auto shadow-lg bg-neutral-800 p-3 rounded-lg border border-yellow-500'>
                     <div className='text-5xl text-white font-bold mb-5'>New Post</div>
                     <form>
+                        {image && (
+                            <Image src={URL.createObjectURL(image)} width={50} height={50} alt="Cover Image" className='my-2 object-cover object-center w-full h-24 rounded-lg' />
+                        )}
                         <input onChange={onChange} name='title' placeholder='Title...' value={post.title} className='w-full px-3 py-1.5 rounded-lg bg-neutral-900 text-white'/>
                         <SimpleMDE 
                             value={post.content}
@@ -76,6 +102,8 @@ const CreatePost: FC = () => {
                             placeholder='Content...'
                             className='mt-3'
                         />
+                        <input type="file" ref={imageFileInput} onChange={handleChange} className='invisible w-0 h-0'/>
+                        <button type='button' onClick={uploadImage} className='w-full text-center py-2 bg-black rounded-md text-white font-medium text-lg mt-4 hover:opacity-90'>Upload Cover Image</button>
                         <button type='button' onClick={createPost} className='w-full text-center py-2 bg-yellow-500 rounded-md text-white font-medium text-lg mt-4 hover:opacity-90'>Create Post</button>
                     </form>
                 </div>
